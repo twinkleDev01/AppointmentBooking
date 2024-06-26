6760b9a6ddba090ca5b389b80743cd75ff743da2
twinkledewangan <twinkledewangan@resiliencesoft.com>
2024-06-26 13:52:02 +0530

    date formatted

--- src/app/feature-module/patients/patient-invoice/patient-invoice.component.ts
+++ src/app/feature-module/patients/patient-invoice/patient-invoice.component.ts
@@ -23,4 +23,17 @@ export class PatientInvoiceComponent {
       this.invoices=res;
     })
   }
+  formatDate(dateString:any) {
+    const date = new Date(dateString);
+  
+    const day = date.getDate();
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear();
+  
+    return `${day} ${month} ${year}`;
+  }
 }
